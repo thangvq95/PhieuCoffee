474dedfc7e90d404689264b00e0d7191446a704e
Vũ Quốc Thắng <thangvq95@gmail.com>
2019-03-28 10:11:18 +0700

    change folder name

---
+++ phieucaphe_mobile_android/app/src/main/java/com/phieucaphe/activity/BranchActivity.kt
@@ -0,0 +1,13 @@
+package com.phieucaphe.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.phieucaphe.R
+
+class BranchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_branch)
+    }
+}
